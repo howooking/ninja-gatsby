7f90a5acdc8408765ce244ddb7c835a51d2b1838
howooking <junsgk@gmail.com>
2023-01-12 18:36:19 +0900

    cap L change

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 import Button from "../components/Button";
 import { graphql } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
